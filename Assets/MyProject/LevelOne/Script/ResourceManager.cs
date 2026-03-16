9200eec371111de2b9d31c8e7f88c49151e18dc3
Mahfoud <binsabbah2013@gmail.com>
2026-03-16 04:18:27 +0300

    fix osama wood tree level

--- Assets/MyProject/LevelOne/Script/ResourceManager.cs
+++ Assets/MyProject/LevelOne/Script/ResourceManager.cs
@@ -4,21 +4,26 @@ using TMPro;
 public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager instance;
-    public TextMeshProUGUI woodText; 
+  //  public TextMeshProUGUI woodText; 
     private int woodAmount = 0;
 
     void Awake() { instance = this; }
 
-    void Start() { UpdateUI(); }
+   // void Start() { UpdateUI(); }
 
     public void AddWood(int amount)
     {
-        woodAmount += amount;
-        UpdateUI();
-    }
-
-    void UpdateUI()
-    {
-        if (woodText != null)
-            woodText.text = "" + woodAmount;
+       // woodAmount += amount;
+         // ✅ Add coins to GameManager
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AddCoins(amount);
+        }
+      //  UpdateUI();
     }}
+
+    // void UpdateUI()
+    // {
+    //     if (woodText != null)
+    //         woodText.text = "" + woodAmount;
+    // }}
